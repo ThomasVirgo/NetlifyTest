f476fd2d605131157a368dfa317780ac272e3f8a
Thomas Virgo <tomcvirgo@gmail.com>
2021-03-28 18:56:12 +0100

    added heading to test CD

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Testing deploy of react app to netlify...</h1>
+      <h2>Added this to check continuous development is working.</h2>
     </div>
   );
 }
